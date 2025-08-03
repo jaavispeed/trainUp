f0e5e143e4035b44b3972289989fc86ad351d927
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-08-02 22:49:21 -0400

    template login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import LoginComponent from './auth/login/login.component';
 import { HeroPageComponent } from './components/hero/hero.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
   {
     path: '**',
