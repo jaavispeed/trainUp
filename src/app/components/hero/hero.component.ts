a257c0b4316d5fc1cdb6611171e46fd30ee7984c
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-08-03 20:11:37 -0400

    feat: instalacion de animate.css

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarPageComponent } from '../../shared/navbar/navbar.component';
 import { ThemeControllerComponent } from '../../shared/theme-controller/theme-controller.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-page',
-  imports: [NavbarPageComponent, ThemeControllerComponent],
+  imports: [NavbarPageComponent, ThemeControllerComponent, RouterLink],
   templateUrl: './hero.component.html',
 })
 export class HeroPageComponent {}
