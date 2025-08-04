de6c9ac38fb4aea1b28001ef6023d359b193d099
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-08-03 21:36:37 -0400

    feat: componente prices

---
+++ src/app/components/free-price/free-price.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-free-price',
+  imports: [],
+  templateUrl: './free-price.component.html',
+})
+export class FreePriceComponent {
+
+}
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarPageComponent } from '../../shared/navbar/navbar.component';
 import { ThemeControllerComponent } from '../../shared/theme-controller/theme-controller.component';
 import { RouterLink } from '@angular/router';
+import { PremiumPriceComponent } from "../premium-price/premium-price.component";
+import { FreePriceComponent } from '../free-price/free-price.component';
 
 @Component({
   selector: 'app-hero-page',
-  imports: [NavbarPageComponent, ThemeControllerComponent, RouterLink],
+  imports: [NavbarPageComponent, ThemeControllerComponent, RouterLink, PremiumPriceComponent, FreePriceComponent],
   templateUrl: './hero.component.html',
 })
 export class HeroPageComponent {}
